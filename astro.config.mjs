88c784bb7eeebb6a146983e5841d970f30cde5a3
Florian van der Galiën <hallo@flori.dev>
2025-09-29 17:55:51 +0200

    chore: add Prettier configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import node from '@astrojs/node';
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node'
+import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
-    mode: 'standalone'
+    mode: 'standalone',
   }),
 
   integrations: [vue()],
 
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+})
