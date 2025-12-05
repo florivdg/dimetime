e5ea35e2a37d405fb048948a4338d1169dbe968e
Florian van der Galiën <hallo@flori.dev>
2025-12-05 19:11:48 +0100

    feat(pagination): update pagination labels to German translations

--- src/components/ui/pagination/PaginationFirst.vue
+++ src/components/ui/pagination/PaginationFirst.vue
@@ -38,7 +38,7 @@ const forwarded = useForwardProps(delegatedProps)
   >
     <slot>
       <ChevronLeftIcon />
-      <span class="hidden sm:block">First</span>
+      <span class="hidden sm:block">Erste</span>
     </slot>
   </PaginationFirst>
 </template>
